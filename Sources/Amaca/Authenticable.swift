74f534be3dea8635ad11829c7c4d5281702ef137
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2022-07-24 14:59:04 -0500

    Basic restful client implementation

---
+++ Sources/Amaca/Authenticable.swift
@@ -0,0 +1,49 @@
+//
+//  Authenticable.swift
+//  FishApp
+//
+//  Created by Luis Ezcurdia on 24/07/22.
+//
+
+import Foundation
+
+public protocol Authenticable {
+    func headers() -> [String: String]
+    func queryItems() -> [String: String]
+}
+
+public struct HeaderAuthentication: Authenticable {
+    let method: String
+    let token: String
+
+    public init(token: String, method: String = "Bearer") {
+        self.token = token
+        self.method = method
+    }
+
+    public func headers() -> [String: String] {
+        return ["Authentication": "\(method) \(token)"]
+    }
+
+    public func queryItems() -> [String: String] {
+        return [:]
+    }
+}
+
+public struct QueryAuthentication: Authenticable {
+    let key: String
+    let token: String
+
+    public init(token: String, key: String = "token") {
+        self.token = token
+        self.key = key
+    }
+
+    public func headers() -> [String: String] {
+        return [:]
+    }
+
+    public func queryItems() -> [String: String] {
+        return [key: token]
+    }
+}
---
+++ Sources/Amaca/Client.swift
@@ -0,0 +1,84 @@
+//
+//  Client.swift
+//  FishApp
+//
+//  Created by Luis Ezcurdia on 23/07/22.
+//
+
+import Foundation
+
+public enum NetworkError: Error {
+    case invalidRequest(String)
+    case invalidResponse(String)
+    case clientError(String)
+    case serverError(String)
+}
+
+public struct Client {
+    let baseUrl: String
+    let session: URLSession
+    var contentMode = Request.ContentMode.json
+
+    public init(_ baseUrl: String, session: URLSession = URLSession.shared) {
+        self.baseUrl = baseUrl
+        self.session = session
+    }
+
+    public func get(path: String = "/", queryItems: [String: String]? = nil, headers: [String: String]? = nil) async throws -> Data? {
+        return try await request(method: "get", path: path, queryItems: queryItems, headers: headers)
+    }
+
+    public func post(path: String = "/", body: Data? = nil, queryItems: [String: String]? = nil, headers: [String: String]? = nil) async throws -> Data? {
+        return try await request(method: "post", path: path, body: body, queryItems: queryItems, headers: headers)
+    }
+
+    public func put(path: String = "/", body: Data? = nil, queryItems: [String: String]? = nil, headers: [String: String]? = nil) async throws -> Data? {
+        return try await request(method: "put", path: path, body: body, queryItems: queryItems, headers: headers)
+    }
+
+    public func patch(path: String = "/", body: Data? = nil, queryItems: [String: String]? = nil, headers: [String: String]? = nil) async throws -> Data? {
+        return try await request(method: "patch", path: path, body: body, queryItems: queryItems, headers: headers)
+    }
+
+    public func delete(path: String = "/", queryItems: [String: String]? = nil, headers: [String: String]? = nil) async throws -> Data? {
+        return try await request(method: "delete", path: path, queryItems: queryItems, headers: headers)
+    }
+
+    public func request(method: String, path: String = "/", body: Data? = nil,
+                 queryItems: [String: String]? = nil, headers: [String: String]? = nil) async throws -> Data? {
+        var req = Request(baseUrl: self.baseUrl)
+        req.method = method
+        req.path = path
+        req.body = body
+        req.queryItems = queryItems
+        req.contentMode = contentMode
+        req.headers = headers
+        guard let urlRequest = req.urlRequest else {
+            throw NetworkError.invalidRequest("Invalid \(method) request for \(path)")
+        }
+        return try await request(urlRequest)
+    }
+
+    public func request(_ req: URLRequest) async throws -> Data? {
+        let (data, response) = try await session.data(for: req)
+
+        guard let httpResponse = response as? HTTPURLResponse else {
+            throw NetworkError.invalidResponse("Invalid http url response")
+        }
+
+        switch StatusCode(rawValue: httpResponse.statusCode) {
+        case .success:
+            return data
+        case .clientError:
+            throw NetworkError.clientError("Client error code: \(httpResponse.statusCode)")
+        case .serverError:
+            throw NetworkError.serverError("Server error code: \(httpResponse.statusCode)")
+        default:
+            #if DEBUG
+            debugPrint(httpResponse)
+            debugPrint(data)
+            #endif
+            return nil
+        }
+    }
+}
